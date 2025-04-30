2edbbc2f7c4f039722ad50d7399bf2f9cf9a7203
Alejandro Luther Dominguez <aluther-@car11s1.42barcelona.com>
2025-04-30 15:00:17 +0200

    ex11 done. Los punteros se me siguen resistiendo un monton. Tendre que seguir peleandome con ellos

---
+++ C-piscine-reloaded/ex11/ft_div_mod.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 14:05:28 by aluther-          #+#    #+#             */
+/*   Updated: 2025/04/30 14:46:18 by aluther-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+		*div = a / b;
+		*mod = a % b;
+}
+
+#include <stdio.h>
+int main()
+{
+	int a = 10;
+	int b = 5;
+	int div;
+	int mod;
+	
+	ft_div_mod(a, b, &div, &mod);
+	printf("Divisor %d\n", div);
+	printf("residuo %d\n", mod);
+	return 0;
+}
