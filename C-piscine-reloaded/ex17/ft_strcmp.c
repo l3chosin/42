d1111a734240722e6691a7d66634a115c81d02f3
Alejandro Luther Dominguez <aluther-@car11s2.42barcelona.com>
2025-05-01 15:49:48 +0200

    joder puto compare

--- C-piscine-reloaded/ex17/ft_strcmp.c
+++ C-piscine-reloaded/ex17/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 15:06:33 by aluther-          #+#    #+#             */
-/*   Updated: 2025/05/01 15:14:56 by aluther-         ###   ########.fr       */
+/*   Updated: 2025/05/01 15:36:28 by aluther-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,22 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	if (s1[i] == s2[i])
-		i++;
-	return (s1 - s2);
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		if (s1[i] != s2[i])
+			return (s1 - s2);
+	}
+	return (0);
 }
 
-#include <stdio.h>
-int	main()
+/*#include <stdio.h>
+int     main()
 {
-	char s1[] = "Hola";
-	char s2[] = "Hola";
-	int result = ft_strcmp(s1, s2);
-	printf("Resultado %d", result);
-	return 0;
-}
+        char s1[] = "a";
+        char s2[] = "Hola";
+        int result = ft_strcmp(s1, s2);
+        printf("Resultado %d", result);
+        return 0;
+}*/
