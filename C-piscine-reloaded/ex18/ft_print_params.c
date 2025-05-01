05fe594bce4c603a1142b61bc6c7572ae439bfcf
Alejandro Luther Dominguez <aluther-@car10s3.42barcelona.com>
2025-05-01 19:07:47 +0200

    error en la putchar

--- C-piscine-reloaded/ex18/ft_print_params.c
+++ C-piscine-reloaded/ex18/ft_print_params.c
@@ -10,12 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+
+void	ft_putchar(char c);
 
 int	main(int argc, char *argv[])
 {
