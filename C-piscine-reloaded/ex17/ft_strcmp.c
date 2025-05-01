7ca663653147ad031ceb7081b30d762cc6eab479
Alejandro Luther Dominguez <aluther-@car10s3.42barcelona.com>
2025-05-01 17:34:51 +0200

    Correcion 17 cortesia de copilot

--- C-piscine-reloaded/ex17/ft_strcmp.c
+++ C-piscine-reloaded/ex17/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 15:06:33 by aluther-          #+#    #+#             */
-/*   Updated: 2025/05/01 15:36:28 by aluther-         ###   ########.fr       */
+/*   Updated: 2025/05/01 17:34:19 by aluther-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,19 @@ int	ft_strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
-			i++;
 		if (s1[i] != s2[i])
-			return (s1 - s2);
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
 
-/*#include <stdio.h>
+#include <stdio.h>
 int     main()
 {
-        char s1[] = "a";
+        char s1[] = "Hola";
         char s2[] = "Hola";
         int result = ft_strcmp(s1, s2);
         printf("Resultado %d", result);
         return 0;
-}*/
+}
