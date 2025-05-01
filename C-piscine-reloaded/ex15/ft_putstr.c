0b6596bf350e7e893bf0a2763f0c5535b433abde
Alejandro Luther Dominguez <aluther-@car11s2.42barcelona.com>
2025-05-01 14:54:24 +0200

    Vamos jose que 14 y 15 funcionan vamos vamos vamos VAMOSS

---
+++ C-piscine-reloaded/ex15/ft_putstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 14:43:29 by aluther-          #+#    #+#             */
+/*   Updated: 2025/05/01 14:53:35 by aluther-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}*/
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+/*int	main()
+{
+	char str[] = "Ey brodel como te encuentras \n cabr0009988nnaso";
+
+	ft_putstr(str);
+	return 0;
+}*/
