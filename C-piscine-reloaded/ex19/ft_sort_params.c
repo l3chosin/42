02f567e26db364e4088da53e2d2d10ad69b5715c
Alejandro Luther Dominguez <aluther-@car10s3.42barcelona.com>
2025-05-01 19:50:30 +0200

    Joder

---
+++ C-piscine-reloaded/ex19/ft_sort_params.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 19:12:33 by aluther-          #+#    #+#             */
+/*   Updated: 2025/05/01 19:16:46 by aluther-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+int	main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int temp;
+	int result;
+	j = 1;
+	while (j <= argc)
+		{
+
+		}
+	return(0);
+}
