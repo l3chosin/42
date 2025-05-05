4dfe1de067885a172b5e7b442947dac1db203e78
Alejandro Luther Dominguez <aluther-@car11s3.42barcelona.com>
2025-05-05 16:49:30 +0200

    diosmio bendito lo he conseguido. Eso si con ayuda de companeros y copilot. Aun me cuesta bastante este codigo, ademas de que es bastante bastante largo. Creo que me pondre una alarma e ire probando cada semana a hacerlo por mi cuenta hasta dejar de depender de las IAs

--- C-piscine-reloaded/ex19/ft_sort_params.c
+++ C-piscine-reloaded/ex19/ft_sort_params.c
@@ -16,9 +16,9 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_swap(int *a, int *b)
+void	ft_swap(char **a, char **b)
 {
-	int	temp;
+	char	*temp;
 
 	temp = *a;
 	*a = *b;
@@ -39,8 +39,42 @@ int	ft_strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
+void	ft_sort_args(int argc, char *argv[])
+{
+	int	j;
+	int	sorted;
+
+	sorted = 0;
+	while (sorted == 0)
+	{
+		sorted = 1;
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				ft_swap(&argv[j], &argv[j + 1]);
+				sorted = 0;
+			}
+			j++;
+		}
+	}
+}
+
 int	main(int argc, char *argv[])
 {
+	int	i;
+	int	j;
 
-	return(0);
+	ft_sort_args(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+			ft_putchar(argv[i][j++]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
 }
