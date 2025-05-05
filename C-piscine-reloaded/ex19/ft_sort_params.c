8da1003a1e0b04a195e06cf73508761b26f13bf2
Alejandro Luther Dominguez <aluther-@car11s3.42barcelona.com>
2025-05-05 16:51:47 +0200

    Cambio de la norminette

--- C-piscine-reloaded/ex19/ft_sort_params.c
+++ C-piscine-reloaded/ex19/ft_sort_params.c
@@ -10,11 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+/*#include <unistd.h>
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
+	}*/
+
+void	ft_putchar(char c);
 
 void	ft_swap(char **a, char **b)
 {
