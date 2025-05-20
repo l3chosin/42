d14a7034a6b62d92ff28f6ba5436eaa2bd7415b4
Alejandro Luther Dominguez <aluther-@car1s2.42barcelona.com>
2025-05-20 15:00:11 +0200

    joder

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,38 +6,38 @@
 /*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 11:49:30 by aluther-          #+#    #+#             */
-/*   Updated: 2025/05/20 11:49:32 by aluther-         ###   ########.fr       */
+/*   Updated: 2025/05/20 14:59:41 by aluther-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	ft_strlcat(char *dst, char *src, int size)
 {
-		int	i;
-		int j;
-		int	src_size;
-		int dst_size;
+	int	i;
+	int	j;
+	int	src_size;
+	int	dst_size;
 
-		src_size = 0;
-		dst_size = 0;
-		i = 0;
-		j = 0;
-		while (src[src_size] != '\0')
-			src_size++;
-		while (dst[dst_size] != '\0')
-			dst_size++;
-		if (size > dst_size)
+	src_size = 0;
+	dst_size = 0;
+	i = 0;
+	j = 0;
+	while (src[src_size] != '\0')
+		src_size++;
+	while (dst[dst_size] != '\0')
+		dst_size++;
+	if (size > dst_size)
+	{
+		while (i < dst_size)
+			i++;
+		while (i >= dst_size && src[j] != '\0')
 		{
-			while (i < dst_size)
-				i++;
-			while (i >= dst_size && src[j] != '\0')
-			{
-				dst[i] = src[j];
-				i++;
-				j++;
-			}
-			dst[i] = '\0';
+			dst[i] = src[j];
+			i++;
+			j++;
 		}
-		return (src_size + dst_size);
+		dst[i] = '\0';
+	}
+	return (src_size + dst_size);
 }
 
 /*#include<stdio.h>
