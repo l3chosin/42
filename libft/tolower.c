1b4a7748f093ff279e952f32751fc2ef0ce029a7
Alejandro Luther Dominguez <aluther-@car5s4.42barcelona.com>
2025-05-21 11:51:50 +0200

    FECILISTIMA

--- libft/tolower.c
+++ libft/tolower.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+void	ft_tolower(char *str)
+{
+	int i;
 
+	i = 0;
+	while(str[i])
+	{
+		if (ft_isalpha(str[i]) != 0)
+			{
+				if (str[i] > 64 && str[i] < 91)
+					str[i] += 32;
+			}
+		i++;
+	}
+}
+
+/*#include<stdio.h>
+int	main()
+{
+	char str[] = "HOLAMUNDO. . .   !2@#45^78'''s[[s'???";
+
+	ft_tolower(str);
+ 	printf("Resultado: %s", str);
+  return 0;
+}*/
