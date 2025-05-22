74e8897aef0d083bbcfdb977e702e55f100141ef
Alejandro Luther <alexalexis79@gmail.com>
2025-05-22 15:33:26 +0200

    no

--- libft/incompletas/memchr.c
+++ libft/incompletas/memchr.c
@@ -1,1 +1,47 @@
-void *ft_memchr(void *ptr, int value, size_t num)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memchr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aluther- <aluther-@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 12:14:53 by aluther-          #+#    #+#             */
+/*   Updated: 2025/05/22 12:27:06 by aluther-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(void *str, int ch, int n)
+{
+	int			i;
+	unsigned char		*ptr;
+
+	ptr = (unsigned char *)str;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char) ch)
+			return (&ptr[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+/*#include<stdio.h>
+int main()
+{
+    char str[] = "Hola mundo!";
+    int ch = 'M';
+    int n = 6;
+    char *ptr;
+
+    ptr = ft_memchr(str, ch, n);
+
+    if (ptr != 0)
+        printf("El carácter '%c' encontrado 
+en la posición: %ld\n", ch, ptr - str);
+    else
+        printf("El carácter '%c' no se encuentra en la cadena.\n", ch);
+    return 0;
+    }*/
