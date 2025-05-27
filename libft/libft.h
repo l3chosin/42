ea063fba187acb5809af49982e918f838114f006
Alejandro Luther Dominguez <aluther-@car3s4.42barcelona.com>
2025-05-27 18:43:43 +0200

    itoa funcional

--- libft/libft.h
+++ libft/libft.h
@@ -23,6 +23,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int a);
+char	*ft_itoa(int n);
 void	*ft_memchr(const void *str, int ch, int n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
