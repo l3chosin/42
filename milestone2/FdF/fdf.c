c7000759de8af0beef2e4e4b7fc43eae72f463f0
Alejandro Luther Dominguez <aluther-@cbr8s6.42barcelona.com>
2025-08-11 15:12:47 +0200

    he conseguido abrir una ventana e imprimir una fokin linea

--- milestone2/FdF/fdf.c
+++ milestone2/FdF/fdf.c
@@ -1,14 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 14:51:26 by aluther-          #+#    #+#             */
+/*   Updated: 2025/08/11 14:51:29 by aluther-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
 int	main(void)
 {
-	mlx_t*	mlx;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	int i;
+	int lenght;
 
-	mlx = mlx_init(1920, 1080, "Ekisde", true);
+	lenght = 200;
+	i = 0;
+	mlx = mlx_init(1920, 1080, "testing", false);
 	if (!mlx)
 	{
 		return (EXIT_FAILURE);
 	}
+	img = mlx_new_image(mlx, 256, 256);
+	if (!img)
+		return (EXIT_FAILURE);
+	while (i < lenght)
+	{
+		mlx_put_pixel(img, i, i, 0xffffffff);
+		i++;
+	}
+	mlx_image_to_window(mlx, img, 0, 0);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
