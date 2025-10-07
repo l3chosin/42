c13a7eaefd08191c9aa4a8c53a1d8a4b28632908
Alejandro Luther <alexalexis79@gmail.com>
2025-10-07 13:51:02 +0200

    La funcion entraba en ejecucion a pesar de que estuviera ordenada. Corregido

--- milestone2/push_swap/sorter/ft_sorter.c
+++ milestone2/push_swap/sorter/ft_sorter.c
@@ -30,19 +30,41 @@ t_node	*lowest_node(t_node *stack)
 	return (min);
 }
 
+static	int	first_check(t_node *stack)
+{
+	t_node	*tmp;
+
+	tmp = stack;
+	if (tmp->number > tmp->next->number)
+		return (0);
+	tmp = tmp->next;
+	while (tmp->next != stack)
+	{
+		if (tmp->number > tmp->next->number)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
 void	ft_sorter(t_node **stack_a, t_node **stack_b)
 {
 	int	len;
+	int	sorted;
 
 	len = list_lenght(*stack_a);
-	if (len == 2)
-		two_case(stack_a);
-	else if (len == 3)
-		three_case(stack_a);
-	else if (len == 4)
-		four_case(stack_a, stack_b);
-	else if (len == 5)
-		five_case(stack_a, stack_b);
-	else if (len > 5)
-		big_case(stack_a, stack_b);
+	sorted = first_check(*stack_a);
+	if (sorted == 0)
+	{
+		if (len == 2)
+			two_case(stack_a);
+		else if (len == 3)
+			three_case(stack_a);
+		else if (len == 4)
+			four_case(stack_a, stack_b);
+		else if (len == 5)
+			five_case(stack_a, stack_b);
+		else if (len > 5)
+			big_case(stack_a, stack_b);
+	}
 }
