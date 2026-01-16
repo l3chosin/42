e2e3f7e6427ca3fbaed52e4343ccc3d6196b0598
Alejandro Luther <alexalexis79@gmail.com>
2026-01-16 17:05:37 +0100

    solo he creado dos estructuras y ya

---
+++ milestone3/philo/philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aluther- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/16 16:01:36 by aluther-          #+#    #+#             */
+/*   Updated: 2026/01/16 16:01:38 by aluther-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+typedef struct s_philo
+{
+
+}				t_philo;
+
+typedef struct s_sim
+{
+	int	time_to_eat;
+	int	time_to_die;
+	int	time_to_think;
+}				t_sim;
+
+#endif
