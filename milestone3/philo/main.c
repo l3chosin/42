c723250b185c210d9cdb5df623c47a334ce0328c
Alejandro Luther <alexalexis79@gmail.com>
2026-03-06 21:47:45 +0100

    .

--- milestone3/philo/main.c
+++ milestone3/philo/main.c
@@ -68,8 +68,14 @@ int	main(int ac, char **av)
 		else
 		{
 			sim = prepare_emulation(av);
-			printf("Forks = %i \nPhilosophers = %i\nTime to die = %i\nTime to eat = %i\n Time to sleep = %i\nTime to think = %i\n", sim.forks, sim.table.n_philos, sim.time_to_die, sim.time_to_eat, sim.time_to_sleep, sim.time_to_think);
-			printf("Times philo must eat = %i\n", sim.must_eat);
+			printf("Forks = %i\n", sim.forks);
+			printf("N of philosophers = %i\n", sim.table.n_philos);
+			printf("Time to die = %i\n", sim.time_to_die);
+			printf("Time to eat = %i\n", sim.time_to_eat);
+			printf("Time to sleep = %i\n", sim.time_to_sleep);
+			printf("Time to think = %i\n", sim.time_to_think);
+			if (av[6])
+				printf("Times philo must eat = %i\n", sim.must_eat);
 		}
 	}
 	else
