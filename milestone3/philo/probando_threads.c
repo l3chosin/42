dade68979a174afbf9d1f5638a60a110ade5fc7a
Alejandro Luther <alexalexis79@gmail.com>
2026-03-16 20:02:11 +0100

    Creo que ya sé como hacer los mutex y los threads correctamente. Me toca empezar a crear de verdad la estructura de los filosofos

--- milestone3/philo/probando_threads.c
+++ milestone3/philo/probando_threads.c
@@ -1,4 +1,3 @@
-
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,6 +6,7 @@ typedef struct s_thread_data
 {
     int segundos;
     int id;
+    pthread_mutex_t *mutex;
 } t_thread_data;
 
 
@@ -22,18 +22,21 @@ void *funcion_prueba(void *arg)
     int j = i;
     while (j > 0)
     {
+        pthread_mutex_lock(data->mutex);
         printf("\033[%i;0H", id);          // Ir a la línea del thread
         printf("\033[2K");                  // Limpiar la línea
         printf("Thread %i: %i segundos restantes", id, j);
         fflush(stdout);
+        pthread_mutex_unlock(data->mutex);
         usleep(1 * 1000000);
         j--;
     }
-
+    pthread_mutex_lock(data->mutex);
     printf("\033[%i;0H", id);
     printf("\033[2K");
     printf("Thread %i: terminado!\n", id);
     fflush(stdout);
+    pthread_mutex_unlock(data->mutex);
     return (NULL);
 }
 
@@ -41,15 +44,19 @@ int main(void)
 {
     pthread_t cum;
     pthread_t cum2;
+    pthread_mutex_t mutex;
 
+    pthread_mutex_init(&mutex, NULL);
     printf("\033[2J\033[H");
     fflush(stdout);
 
-    t_thread_data data1 = {5, 1};
-    t_thread_data data2 = {10, 3};
+    t_thread_data data1 = {5, 1, &mutex};
+    t_thread_data data2 = {10, 3, &mutex};
 
     pthread_create(&cum, NULL, funcion_prueba, &data1);
     pthread_create(&cum2, NULL, funcion_prueba, &data2);
     pthread_join(cum, NULL);
     pthread_join(cum2, NULL);
+
+    pthread_mutex_destroy(&mutex);
 }
